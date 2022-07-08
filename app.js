f059d46510a0e8d008c102e64f38caa89b0990d9
codepasty <75074598+codepasty@users.noreply.github.com>
2022-07-08 13:07:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+
+setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "0";
+    document.getElementsByClassName("wag2")[0].style.opacity = "0";
+  },400);
+
+  setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "0";
+    document.getElementsByClassName("wag2")[0].style.opacity = "1";
+  },800);
+
+  setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "1";
+    document.getElementsByClassName("wag2")[0].style.opacity = "0";
+  },1200);
+
+  setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "0";
+    document.getElementsByClassName("wag2")[0].style.opacity = "1";
+  },1600);
+
+  setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "1";
+    document.getElementsByClassName("wag2")[0].style.opacity = "0";
+  },2000);
+
+   setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "0";
+    document.getElementsByClassName("wag2")[0].style.opacity = "1";
+  },2400);
+
+  setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "1";
+    document.getElementsByClassName("wag2")[0].style.opacity = "0";
+  },2800);
+
+   setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "0";
+    document.getElementsByClassName("wag2")[0].style.opacity = "1";
+  },3200);
+   setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "1";
+    document.getElementsByClassName("wag2")[0].style.opacity = "0";
+  },3600);
+
+   setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "0";
+    document.getElementsByClassName("wag2")[0].style.opacity = "1";
+  },4000);
+   setTimeout(function(){
+    document.getElementsByClassName("wag1")[0].style.opacity = "1";
+    document.getElementsByClassName("wag2")[0].style.opacity = "0";
+  },4400);
